f4b381ffae324a3a9f5bb63b85246066194e8266
JameWade <qq1729128876@gmail.com>
2024-10-11 16:37:18 +0800

    first commit

---
+++ database/src/lib.rs
@@ -0,0 +1,221 @@
+use binance::api::Binance;
+use binance::market::Market;
+use binance::model::KlineSummary;
+use sqlx::mysql::{MySqlPoolOptions, MySqlRow};
+use dotenvy::dotenv;
+use std::env;
+use sqlx::Row;
+use tokio::task;
+
+#[cfg_attr(not(debug_assertions), allow(dead_code))]
+#[derive(Debug)]
+#[derive(sqlx::FromRow)]
+struct MyKlineSummary {
+    pub open_time: Option<i64>,
+
+    pub open: Option<String>,
+
+    pub high: Option<String>,
+
+    pub low: Option<String>,
+
+    pub close: Option<String>,
+
+    pub volume: Option<String>,
+
+    pub close_time: Option<i64>,
+
+    pub quote_asset_volume: Option<String>,
+
+    pub number_of_trades: Option<i64>,
+
+    pub taker_buy_base_asset_volume: Option<String>,
+
+    pub taker_buy_quote_asset_volume: Option<String>,
+}
+
+//https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1d&startTime=1346198400000&endTime=1704067200000&limit=1000
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("########GET BTC PRICE!##############");
+
+    // Load environment variables
+    dotenv().ok();
+    match env::var("DATABASE_URL") {
+        Ok(url) => println!("DATABASE_URL: {}", url),
+        Err(_) => println!("DATABASE_URL is not set"),
+    }
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    // Spawn a blocking task to get data from Binance
+    let klines = task::spawn_blocking(move || {
+        let market: Market = Binance::new(None, None);
+        get_all_data_from_online(&market)
+        // get_data_from_online(&market)
+    }).await??;
+
+    // Store the data in the database
+    store_data_in_db(&database_url, klines).await?;
+
+    println!("Data processing completed successfully");
+    Ok(())
+}
+
+fn get_data_from_online(market: &Market) -> Result<Vec<KlineSummary>, binance::errors::Error> {
+    // last 10 1-day klines (candlesticks) for a symbol:
+    let klines = market.get_klines("BTCUSDT", "1d", None, None, None)?;
+
+    match klines {
+        binance::model::KlineSummaries::AllKlineSummaries(klines) => {
+            println!("Fetched {} klines", klines.len());
+            Ok(klines)
+        }
+    }
+}
+
+fn get_all_data_from_online(market: &Market) -> Result<Vec<KlineSummary>, String> {
+    let mut all_klines = Vec::new();
+    let mut start_time: Option<u64> = Some(1502928000000);  //2017.8.17
+    let symbol = "BTCUSDT";
+    let interval = "1d";
+
+
+    loop {
+        match market.get_klines(symbol, interval, Some(1000), start_time, None) {
+            Ok(klines) => match klines {
+                binance::model::KlineSummaries::AllKlineSummaries(mut klines) => {
+                    //println!("klines:{:?}",klines);
+                    if klines.len() < 1000 {
+                        all_klines.append(&mut klines);
+                        break;  // No more data to fetch
+                    }
+
+                    // Get the time of the last kline in this batch
+                    start_time = Some(klines.last().unwrap().close_time as u64);
+
+                    all_klines.append(&mut klines);
+
+                    println!("kline num:{:?}", all_klines.len());
+                }
+            },
+            Err(e) => return Err(format!("Failed to get data: {}", e)),
+        }
+    }
+
+    Ok(all_klines)
+}
+
+pub async  fn store_data_in_db(database_url: &str, data: Vec<KlineSummary>) -> Result<(), sqlx::Error> {
+    let pool = MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect(database_url)
+        .await?;
+
+    for kline in data {
+        sqlx::query!(
+            r#"INSERT INTO btc_daily_data
+               (open_time, open, high, low, close, volume, close_time,
+                quote_asset_volume, number_of_trades, taker_buy_base_asset_volume,
+                taker_buy_quote_asset_volume)
+               VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"#,
+            kline.open_time as i64,
+            kline.open,
+            kline.high,
+            kline.low,
+            kline.close,
+            kline.volume,
+            kline.close_time as i64,
+            kline.quote_asset_volume,
+            kline.number_of_trades,
+            kline.taker_buy_base_asset_volume,
+            kline.taker_buy_quote_asset_volume,
+        )
+            .execute(&pool)
+            .await?;
+    }
+
+    println!("Data stored successfully");
+    Ok(())
+}
+
+pub async fn query_data_from_db(database_url: &str) -> Result<Vec<KlineSummary>, sqlx::Error> {
+    let pool = MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect(database_url)
+        .await?;
+
+    let rows = sqlx::query(
+        r#"SELECT
+            open_time,
+            open,
+            high,
+            low,
+            close,
+            volume,
+            close_time,
+            quote_asset_volume,
+            number_of_trades,
+            taker_buy_base_asset_volume,
+            taker_buy_quote_asset_volume
+        FROM btc_daily_data"#
+    )
+        .fetch_all(&pool)
+        .await?;
+
+    let data = process_rows(rows)?;
+
+    Ok(data)
+}
+fn process_rows(rows: Vec<sqlx::mysql::MySqlRow>) -> Result<Vec<KlineSummary>, sqlx::Error> {
+    let mut data = Vec::new();
+
+    for row in rows.iter() {
+        let kline_summary = KlineSummary {
+            open_time: parse_i64(row.try_get("open_time"), 0),
+            open: get_string(row, "open"),
+            high: get_string(row, "high"),
+            low: get_string(row, "low"),
+            close: get_string(row, "close"),
+            volume: get_string(row, "volume"),
+            close_time: parse_i64(row.try_get("close_time"), 0),
+            quote_asset_volume: get_string(row, "quote_asset_volume"),
+            number_of_trades: parse_i64(row.try_get("number_of_trades"), 0),
+            taker_buy_base_asset_volume: get_string(row, "taker_buy_base_asset_volume"),
+            taker_buy_quote_asset_volume: get_string(row, "taker_buy_quote_asset_volume"),
+        };
+
+        data.push(kline_summary);
+    }
+
+    Ok(data)
+}
+// 辅助函数：安全地解析 i64，出错时返回默认值
+fn parse_i64(value: Result<i64, sqlx::Error>, default: i64) -> i64 {
+    value.unwrap_or(default)
+}
+
+// 辅助函数：安全地获取字符串，出错时返回空字符串
+fn get_string(row: &MySqlRow, index: &str) -> String {
+    row.try_get(index).unwrap_or_else(|_| String::new())
+}
+#[cfg(test)]
+mod test {
+    use std::env;
+    use dotenvy::dotenv;
+    use crate::query_data_from_db;
+
+    #[tokio::test]
+    async fn test_btc_day_data()-> Result<(), Box<dyn std::error::Error>> {
+        // Add your test implementation here
+        dotenv().ok();
+
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+        let data = query_data_from_db(&database_url).await?;
+        data.iter().for_each(|day_data|{
+            println!("{:?}", day_data);
+
+        });
+        Ok(())
+    }
+}
---
+++ rust_binance_bot/src/main.rs
@@ -0,0 +1,100 @@
+use std::env;
+use binance::model::KlineSummary;
+use csv::ReaderBuilder;
+use dotenvy::dotenv;
+use linfa::Dataset;
+use linfa::metrics::ToConfusionMatrix;
+use linfa::traits::{Fit, Predict};
+use linfa_bayes::GaussianNb;
+use ndarray::{Array, Array1, Array2, ArrayBase, Ix1, Ix2, OwnedRepr, s};
+use ndarray_csv::Array2Reader;
+use database::query_data_from_db;
+//camp model
+#[tokio::main]
+async fn main()-> Result<(), Box<dyn std::error::Error>> {
+    println!("Hello, world!");
+
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let data = query_data_from_db(&database_url).await?;
+    deal_data(data);
+    // data.iter().for_each(|day_data|{
+    //     println!("{:?}", day_data);
+    //
+    // });
+
+    Ok(())
+
+}
+
+fn deal_data(data: Vec<KlineSummary>) {
+    let  features: Vec<f64> = data.iter()
+        .skip(1)
+        .flat_map(|kline| vec![
+            kline.close.parse::<f64>().unwrap_or(0.0),
+            kline.volume.parse::<f64>().unwrap_or(0.0),
+        ])
+        .collect();
+
+    // 目标数据：将下一个时间段的收盘价与当前收盘价比较，高于则为1，否则为0
+    let targets: Vec<usize> = data.windows(2)
+        .map(|window| {
+            let current_close = window[0].close.parse::<f64>().unwrap_or(0.0);
+            let next_close = window[1].close.parse::<f64>().unwrap_or(0.0);
+            if next_close > current_close { 1 } else { 0 }
+        })
+        .collect();
+
+    // 确保特征和目标的数量匹配
+    let features_len = features.len() / 2;  // 每个KlineSummary有2个特征
+
+    // 将特征数据转换为 ndarray::Array2，并将目标数据转换为 ndarray::Array1
+    let x = Array2::from_shape_vec((features_len, 2), features).unwrap();
+    // let y = Array1::from_vec(targets);
+    let y = Array1::from_shape_vec(targets.len(), targets).unwrap().to_owned();
+
+    let feature_names = vec![
+      "close", "volume",
+    ];
+
+    let training_records = x.mapv(|c| c); ///to_owned一样
+
+    println!(
+        "We obtain a {}x{} matrix of counts for the vocabulary entries",
+        training_records.dim().0,
+        training_records.dim().1
+    );
+
+    let training_dataset:Dataset<f64, usize, Ix1> = (training_records, y).into();
+
+    // 创建训练集
+    let training_dataset =  training_dataset
+        .map_targets(|x| {
+            if *x == 1 {
+                "accepted"
+            } else {
+                "denied"
+            }
+        })
+        .with_feature_names(feature_names);
+
+
+    print!("???????????{:?}",training_dataset);
+    let model = GaussianNb::params().fit(&training_dataset).unwrap();
+
+    let training_prediction = model.predict(&training_dataset);
+    let cm = training_prediction
+        .confusion_matrix(&training_dataset)
+        .unwrap();
+    // 0.9944
+    let accuracy = cm.f1_score();
+    println!("The fitted model has a training f1 score of {}", accuracy);
+
+}
+
+
+
+
+
